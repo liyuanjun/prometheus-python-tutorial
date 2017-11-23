1703b2b13567b554cad6f4c4417285839a077a1a
tom <liyuanjunmail@qq.com>
2017-11-23 16:54:46 +0800

    .

--- test.py
+++ test.py
@@ -11,11 +11,35 @@ from flask import Flask
 
 app = Flask(__name__)
 
+"""
+tom@ThinkPad:~$ curl localhost:5000/metrics
 
-@app.route('/', methods=['GET', 'POST'])
-def hello_world():
-    return 'Hello World!flask'
+# HELP go_gc_duration_seconds A summary of the GC invocation durations.
+# TYPE go_gc_duration_seconds summary
+go_gc_duration_seconds{quantile="0"} 2.5483e-05
+go_gc_duration_seconds{quantile="0.25"} 3.4437e-05
+go_gc_duration_seconds{quantile="0.5"} 4.0085e-05
+go_gc_duration_seconds{quantile="0.75"} 5.2158e-05
+go_gc_duration_seconds{quantile="1"} 0.012065288
+go_gc_duration_seconds_sum 0.022820333
+go_gc_duration_seconds_count 171
+"""
+
+
+@app.route('/metrics', methods=['GET', 'POST'])
+def metrics():
+    return u"""
+# HELP go_gc_duration_seconds A summary of the GC invocation durations.
+# TYPE go_gc_duration_seconds summary
+go_gc_duration_seconds{quantile="0"} 2.5483e-05
+go_gc_duration_seconds{quantile="0.25"} 3.4437e-05
+go_gc_duration_seconds{quantile="0.5"} 4.0085e-05
+go_gc_duration_seconds{quantile="0.75"} 5.2158e-05
+go_gc_duration_seconds{quantile="1"} 0.012065288
+go_gc_duration_seconds_sum 0.022820333
+go_gc_duration_seconds_count 171
+"""
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
